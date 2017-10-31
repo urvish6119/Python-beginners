db229b61244ca84c78c3e5fa8d749310fdaae8de
Urvish P. Patel <urvish132712@gmail.com>
2017-10-31 14:59:49 -0500

    1.Print
    Learn Print in Python

---
+++ 1.Print/Print.py
@@ -0,0 +1,31 @@
+# This line will print Hello World..(I have used " ")
+print("Hello World.. from double quotes")
+# This line will print Hello World..(I have used ' ')
+print('Hello World.. from single quotes')
+# In python it is not necessary to use " " (Double quotes),
+# you can always choose weather you want to use double quote(" " )or single quote(' ')
+# And you can not write print statement starting with single quote and ending with double quote.
+# for example print('Hello World..")
+# Also you can not write print statement starting with double quote and ending with single quote.
+# for example print("Hello World..')
+
+#---------------------------------------------------------------------------------------------------------------#
+# Print arrow
+print('  *  ')
+print(' *** ')
+print('*****')
+print('  *  ')
+print('  *  ')
+print('  *  ')
+
+# print line to separate this steps
+print('------------------------------------------------------')
+# Print pattern
+print('   *   ')
+print('  ***  ')
+print(' ***** ')
+print('*******')
+
+# Thing is in python all things in print function print as it is, It takes space as an input.
+# In C,C++ or JAVA to print such pattern you will need to write looping structure.
+# In C,C++ or JAVA you will be difining class for just one line to print.
