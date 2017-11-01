6ba5e085aa3f027a985a3868ac9235eba1039f36
Urvish P. Patel <urvish132712@gmail.com>
2017-10-31 21:28:19 -0500

    2.math
    Math operators.

---
+++ 2.Math/math.py
@@ -0,0 +1,16 @@
+# In python there are math function included.
+# For example after installing python, open python IDEL and type 2*2 and press enter, You don't need to define variable
+# python supports all operators +,-,*,/ and % to Python on python IDEL.
+# The following line will not be print in output.
+2+2
+# Python takes operators as prefix operations.
+x=2+2-3*1
+print("X is:",x)
+x=2-2+1*3
+print("X is:",x)
+x=1*3-2+2
+print("X is:",x)
+# But it also supports "()" strategy, Where you can finish that operation first and after that, related to that operator.
+# for example: here it is taking operation (3-1) first and then taking 2*(2).
+x=2+2*(3-1)
+print("X is:",x)
