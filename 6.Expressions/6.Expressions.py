03a9960a61a74fa22a5b2779fb2078cdc1953add
Urvish P. Patel <urvish132712@gmail.com>
2017-11-06 23:20:50 -0600

    6.Expressions
    learn expressions: +,-,/,%,*,**,//

---
+++ 6.Expressions/6.Expressions.py
@@ -0,0 +1,29 @@
+# Now it's time to learn some expressions in python
+# all expressions:
+# x+y: sum of x and y if numbers and concatenated if string.
+# x-y: minus y from x.
+# x*y: multiply x times y, if one of them is number then it will concatenated same number times another number.
+# x/y: divide x by y
+# x//y: floor of x divided by y
+# x%y: remainder of x divided by y
+# x**y: x rais to power y
+# examples:
+x=5
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+# With python it is also possible to floating operations using real numbers.
+one=1
+onethird=1/3
+remain=one-onethird;
+print(remain)
+# Mixed type arithmentics:
+x=4
+y=4.5
+z=x+y
+print(z)
